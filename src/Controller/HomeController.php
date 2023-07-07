6ee14622b93b2182f4803dc8495cb0366e0a7d95
Milbrin <favouille@gmail.com>
2023-07-07 13:42:22 +0200

    fetch products and display them

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $repository = $em->getRepository(Product::class);
+        $products = $repository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'products'=> $products
         ]);
     }
 }
