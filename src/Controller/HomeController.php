f276ea3f4e57305d7f8ee09a5d709859f733f6ba
Milbrin <favouille@gmail.com>
2023-07-10 15:06:07 +0200

    cart total working

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,25 +21,34 @@ class HomeController extends AbstractController
         $productRepository = $em->getRepository(Product::class);
 
         // Temporary, until we have real authentification
+        // We get the first user of BDD to use it as current user
         $userRepository = $em->getRepository(User::class);
         $user = $userRepository->findAll()[0];
 
         $products = $productRepository->findAll();
 
+        // We search if existing user's cart
         $cartRepository = $em->getRepository(Cart::class);
         $cart = $cartRepository->findOneBy(array('user' => $user->getId()));
+
+        // If not we create a new one for this user
         if(!$cart) {
             $cart = new Cart();
             $cart->setUser($user);
+            $cart->setTotal(0);
             $cart->setCreatedAt(new DateTimeImmutable());
             $cart->setUpdatedAt(new DateTimeImmutable());
         }
 
+        // If method is POST, user is adding one item to cart
         if($request->getMethod() === 'POST') {
+            // We get the product id in post params and search the product
             $product_id = $request->get('product-id');
             $product = $productRepository->findOneById($product_id);
+            // We search if in this cart, this product is already added
             $cartItemRepository = $em->getRepository(CartItem::class);
             $cartItem = $cartItemRepository->findOneBy(array('product' => $product->getId(), 'cart' => $cart->getId()));
+            // If not we create one with quantity 1
             if(!$cartItem) {
                 $cartItem = new CartItem();
                 $cartItem->setCart($cart);
@@ -48,11 +57,13 @@ class HomeController extends AbstractController
                 $cartItem->setCreatedAt(new DateTimeImmutable());
             }
             else {
+                // Else if it already exists, we add 1 to his quantity
                 $cartItem->setQuantity($cartItem->getQuantity() + 1);
             }
     
             $cartItem->setUpdatedAt(new DateTimeImmutable());
-            $cart->setTotal($cartItem->getQuantity() * $product->getPrice());
+            // We update the total of cart with the product price
+            $cart->setTotal($cart->getTotal() + $product->getPrice());
             $em->persist($cartItem);
         }
         $em->persist($cart);
